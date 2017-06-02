0bb524d2d7b59eba0e78725fb0260685e511436c
Simone Vittori <hello@simonewebdesign.it>
2017-06-02 14:00:59 +0100

    Add FizzContext skeleton

---
+++ lib/fizzbuzz/fizz_context.ex
@@ -0,0 +1,23 @@
+defmodule FizzBuzz.FizzContext do
+
+  def list_items do
+  end
+
+  def change_item(%FizzBuzz.FizzContext.Item{} = item) do
+  end
+
+  def create_item(item_params) do
+  end
+
+  def get_item!(id) do
+  end
+
+  def change_item(item) do
+  end
+
+  def update_item(item, item_params) do
+  end
+
+  def delete_item(item) do
+  end
+end
