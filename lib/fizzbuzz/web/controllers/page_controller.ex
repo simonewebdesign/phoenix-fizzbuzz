94185812bbb92596a1d3b8f2595a6bb38ae399ca
Simone Vittori <hello@simonewebdesign.it>
2017-06-02 13:20:33 +0100

    Display the first 100 values

--- lib/fizzbuzz/web/controllers/page_controller.ex
+++ lib/fizzbuzz/web/controllers/page_controller.ex
@@ -2,6 +2,6 @@ defmodule FizzBuzz.Web.PageController do
   use FizzBuzz.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    render conn, "index.html", fizzbuzz_sequence: FizzBuzz.perform(100)
   end
 end
