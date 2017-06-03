0eb9aba9bdca7b8ba78cb945187c01ea4093c0d4
Simone Vittori <hello@simonewebdesign.it>
2017-06-03 12:06:07 +0100

    Refactor FizzBuzz to a separate function

--- lib/fizzbuzz.ex
+++ lib/fizzbuzz.ex
@@ -9,14 +9,26 @@ defmodule FizzBuzz do
   ["1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz"]
   """
   @spec perform(integer) :: [String.t]
-  def perform(n) do
-    Enum.map(1..n, fn i ->
-      cond do
-        rem(i, 3) == 0 && rem(i, 5) == 0 -> "FizzBuzz"
-        rem(i, 3) == 0 -> "Fizz"
-        rem(i, 5) == 0 -> "Buzz"
-        true -> to_string i
-      end
-    end)
+  def perform(n), do: Enum.map(1..n, &fizzbuzz/1)
+
+  @doc """
+  Returns the FizzBuzz value of a number.
+
+  ## Examples
+
+  iex> FizzBuzz.fizzbuzz(3)
+  "Fizz"
+
+  iex> FizzBuzz.fizzbuzz(5)
+  "Buzz"
+  """
+  @spec fizzbuzz(integer) :: String.t
+  def fizzbuzz(i) do
+    cond do
+      rem(i, 3) == 0 && rem(i, 5) == 0 -> "FizzBuzz"
+      rem(i, 3) == 0 -> "Fizz"
+      rem(i, 5) == 0 -> "Buzz"
+      true -> to_string i
+    end
   end
 end
