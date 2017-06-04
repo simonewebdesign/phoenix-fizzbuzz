0a1b629a1cc6283f19077e8812bb3ab4083f3876
Simone Vittori <hello@simonewebdesign.it>
2017-06-04 12:53:51 +0100

    Check for ETS entry and display if present

--- lib/fizzbuzz/web/controllers/page_controller.ex
+++ lib/fizzbuzz/web/controllers/page_controller.ex
@@ -2,6 +2,15 @@ defmodule FizzBuzz.Web.PageController do
   use FizzBuzz.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html", fizzbuzz_sequence: FizzBuzz.perform(100)
+    limit = 100
+    sequence = Enum.map(0..limit, &fetch_or_generate_fizzbuzz_item/1)
+    render conn, "index.html", fizzbuzz_sequence: sequence
+  end
+
+  defp fetch_or_generate_fizzbuzz_item(id) do
+    case :ets.lookup(:db, id) do
+      [{_, item}] -> item
+      [] -> FizzBuzz.FizzContext.Item.to_struct(id, FizzBuzz.fizzbuzz(id+1))
+    end
   end
 end
