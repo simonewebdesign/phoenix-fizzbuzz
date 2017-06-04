7c3a43957bad8ed54e0476ba57904bac2a83c211
Simone Vittori <hello@simonewebdesign.it>
2017-06-04 17:40:49 +0100

    Pagination via query params

--- lib/fizzbuzz/web/controllers/page_controller.ex
+++ lib/fizzbuzz/web/controllers/page_controller.ex
@@ -1,11 +1,23 @@
 defmodule FizzBuzz.Web.PageController do
   use FizzBuzz.Web, :controller
 
-  def index(conn, _params) do
-    limit = 100
-    sequence = Enum.map(0..limit, &fetch_or_generate_fizzbuzz_item/1)
+  @page_size 100
+
+  def index(conn, params) do
+    page = page_number(params)
+    offset = (page - 1) * @page_size
+    limit = @page_size + offset - 1
+    sequence = Enum.map(offset..limit, &fetch_or_generate_fizzbuzz_item/1)
     render conn, "index.html", fizzbuzz_sequence: sequence
   end
+
+  defp page_number(%{"page" => p}) do
+    case Integer.parse p do
+      {int, _rem} -> if int > 0, do: int, else: 1
+      :error -> 1
+    end
+  end
+  defp page_number(_params), do: 1
 
   defp fetch_or_generate_fizzbuzz_item(id) do
     case :ets.lookup(:db, id) do
